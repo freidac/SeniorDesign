b77a08385f655109e2c4f475f92deaceb230c56c
Freida <fc138@msstate.edu>
2015-04-09 22:09:10 -0500

    Added the cm to px conversion as well as an array in the main file that myview and score class will use.

--- app/src/main/java/seniordesign/seniordesign/MainActivity.java
+++ app/src/main/java/seniordesign/seniordesign/MainActivity.java
@@ -9,10 +9,12 @@ import android.content.Intent;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static float[] target_shots = new float[80];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        insert_coordinates();
         setContentView(R.layout.activity_main);
     }
 
@@ -48,5 +50,29 @@ public class MainActivity extends ActionBarActivity {
         startActivity(n_intent);
     }
 
+    public void insert_coordinates(){
+        //for (int i = 0 ; i<target_shots.length ; i+2)
+        //{
+        //I will call the draw function after filling these up, there are at most 17 bulets in a clip, so a space for x, y cordinates = 34 possible spaces to fill from a full gun load.
+        // }
+        //hard coded for now
+        target_shots[0] = (float) 2.54;
+        // Log.i("Where it should map", String.valueOf(target_shots[0]));
+        target_shots[1] = 0;
+        target_shots[2] = 0;
+        target_shots[3] = (float) 2.54;
+        target_shots[4] = 0;
+        target_shots[5] = (float) -2.54;
+        target_shots[6] = (float) -2.54;
+        target_shots[7] = 0;
+        target_shots[8] = 0;
+        target_shots[9] = 0;
+        target_shots[10] = (float) -5.08;
+        target_shots[11] = 0;
+        target_shots[12] = (float) -7.62;
+        target_shots[13] = 0;
+        target_shots[14] = 9999;
+
+    }
 
 }
