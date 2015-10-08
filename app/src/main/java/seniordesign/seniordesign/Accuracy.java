d4313265ab153a34b7e9b7ce82bf5461c69b4952
Freida <fc138@msstate.edu>
2015-10-08 11:15:19 -0500

    Option of cm or inch added.

--- app/src/main/java/seniordesign/seniordesign/Accuracy.java
+++ app/src/main/java/seniordesign/seniordesign/Accuracy.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.RadioButton;
+import android.view.View;
 
 
 public class Accuracy extends ActionBarActivity {
@@ -13,8 +15,24 @@ public class Accuracy extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accuracy);
-        calculate_accuracy();
+        //calculate_accuracy();
 
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.Inches:
+                if (checked)
+                    calculate_accuracy(1);
+                    break;
+            case R.id.Centimeters:
+                if (checked)
+                    calculate_accuracy(2);
+                    break;
+        }
     }
 
 
@@ -40,8 +58,9 @@ public class Accuracy extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void calculate_accuracy()
+    public void calculate_accuracy(int num)
     {
+
         final TextView acc_label = (TextView) findViewById(R.id.accurate_percentage);
         final TextView acc_title = (TextView) findViewById(R.id.Accurate_title);
         double x;
@@ -49,19 +68,37 @@ public class Accuracy extends ActionBarActivity {
         double distance;
         double mean_distance = 0 ;
 
+        acc_title.setText("You've shot " + Startsession.num_bullets + " bullets: \n");
 
-       // acc_title.setText("You've shot " + Startsession.num_bullets + " bullets: \n");
-        for (int i = 0 ; i< Startsession.count; i+=2)
+        if(num ==1)
         {
-            x = Startsession.target_shots[i];
-            y = Startsession.target_shots[i+1];
-            distance = Math.sqrt((x*x)+ (y*y));
-            mean_distance += distance;
-        //    acc_label.append("Bullet " + (i/2+1) + ":");
-        //    acc_label.append(String.format( "%.2f", distance) + " cm from center \n");
+            for (int i = 0 ; i< Startsession.count; i+=2)
+            {
+                x = Startsession.target_shots[i];
+                y = Startsession.target_shots[i+1];
+                distance = Math.sqrt((x*x)+ (y*y));
+                distance = distance/2.54;
+                mean_distance += distance;
+                acc_label.append("Bullet " + (i/2+1) + ":");
+                acc_label.append(String.format( "%.2f", distance) + " in from center \n");
+                acc_label.append( "\nOverall accuracy: " + String.format("%.2f",mean_distance/Startsession.num_bullets) + "\n");
+            }
         }
-       // acc_label.append( "\nOverall accuracy: " + String.format("%.2f",mean_distance/Startsession.num_bullets) + "\n");
-
-//smaller the distance the more accurate.
+        else if (num ==2)
+        {
+            for (int i = 0 ; i< Startsession.count; i+=2)
+            {
+                x = Startsession.target_shots[i];
+                y = Startsession.target_shots[i+1];
+                distance = Math.sqrt((x*x)+ (y*y));
+                mean_distance += distance;
+                acc_label.append("Bullet " + (i/2+1) + ":");
+                acc_label.append(String.format( "%.2f", distance) + " cm from center \n");
+                acc_label.append( "\nOverall accuracy: " + String.format("%.2f",mean_distance/Startsession.num_bullets) + "\n");
+            }
+        }
+        else{
+            acc_label.append("CHOOSE UNITS OF MEASUREMENTS!");
+        }
     }
 }
--- app/src/main/java/seniordesign/seniordesign/Startsession.java
+++ app/src/main/java/seniordesign/seniordesign/Startsession.java
@@ -32,6 +32,8 @@ public class Startsession extends ActionBarActivity {
     public static int count = 0;
     public static int num_bullets;
 
+
+
     public void sendBtMsg(String msg2send){
         UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID
         try {
@@ -164,6 +166,7 @@ public class Startsession extends ActionBarActivity {
             }
         }
     }
+
 
 
 
